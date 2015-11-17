d6c85d10f7182894e5afbbd79f68dd8bc9e6780a
Nicholas Olds <nolds9@yahoo.com>
2015-11-17 17:35:18 -0500

    As an admin, I should be able to manage all content

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,10 +2,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-      can :read, Article
-      if user
-        can :create, Article
-      end
+    can :read, Article
+    if user
+      can :create, Article
       can [:update, :destroy], Article, :user => user
+      if user.admin
+        can :manage, Article
+      end
+    end
   end
 end
