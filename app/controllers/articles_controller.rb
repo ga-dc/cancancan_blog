1e4322544d5034e0b39aa9ba9e22c704afb99bec
Nicholas Olds <nolds9@yahoo.com>
2015-11-17 17:11:15 -0500

    As a user or guest, I should be able to read an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,11 @@
 class ArticlesController < ApplicationController
-
   def index
     @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
+    authorize! :read, @article
   end
 
   def new
